6d2b837bb5c1391b94269cb3253baed78166b3f4
李彬 <180505@nd.com>
2020-01-16 20:34:59 +0800

    chore:提交webpack配置及入口文件

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+module.exports = function () {
+  const dev = true;
+  return {
+    mode: dev ? 'development' : 'production',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: dev ? 'axios.js' : 'axios.min',
+      sourceMapFilename: dev ? 'axios.map' : 'axios.min.map',
+      libraryTarget: 'umd'
+    },
+    devtool: 'source-map',
+    // 加载babel进行代码降级
+    module: {
+      rules: [
+        {
+          test: /\.js$/i,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    // 开启 webpack-dev-server 插件
+    devServer: {
+      port: 8000,
+      open: true
+    }
+  }
+}
